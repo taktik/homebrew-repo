2c86f8fbd466254bf1c4e4373460a5af8d75e01c
Antoine Baudoux <ab@taktik.be>
2017-06-14 16:16:01 +0200

    Fixed url

--- Formula/oz.rb
+++ Formula/oz.rb
@@ -1,7 +1,7 @@
 class Oz < Formula
   desc "Ozone command line interface"
   homepage "https://www.taktik.com"
-  url "https://github.com/taktik/homebrew-repo/blob/master/archives/ozone-cli-3.1.0.jar"
+  url "https://raw.githubusercontent.com/taktik/homebrew-repo/master/archives/ozone-cli-3.1.0.jar"
   version "3.1.0"
   sha256 "602224a5d0bcb6060175c61db09cd00a25763bdba0385bc798f1386d14c0f063"
 
