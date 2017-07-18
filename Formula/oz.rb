ce48d188b2f95ffc5c4362a8e41338a169044370
Vladimir Somers <vs@taktik.com>
2017-07-18 16:49:16 +0200

    Update Ozone Client shasum for version 3.1.7.

--- Formula/oz.rb
+++ Formula/oz.rb
@@ -3,7 +3,7 @@ class Oz < Formula
   homepage "https://www.taktik.com"
   url "https://raw.githubusercontent.com/taktik/homebrew-repo/master/archives/ozone-client-cli-3.1.7.jar"
   version "3.1.7"
-  sha256 "a1caf16cae916681aeffb809c872571b9801397d7e72ee43e89df4c336d36bfb"
+  sha256 "75e5047313ea68c5059fdd60e33b61b0acef4675820d6efaee41176dda70727d"
 
 
   depends_on :java => "1.8+"
