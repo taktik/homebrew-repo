c09d87475874759eff3e348ed33a70cdcccac894
Vladimir Somers <vs@taktik.com>
2017-09-25 17:39:45 +0200

    Update Ozone Client version.

--- Formula/oz.rb
+++ Formula/oz.rb
@@ -1,9 +1,9 @@
 class Oz < Formula
   desc "Ozone command line interface"
   homepage "https://www.taktik.com"
-  url "https://raw.githubusercontent.com/taktik/homebrew-repo/master/archives/ozone-client-cli-3.1.8.jar"
-  version "3.1.8"
-  sha256 "acb8a4321099f4d477d268748538b841375601d22e0758e555707dd454a0980c"
+  url "https://raw.githubusercontent.com/taktik/homebrew-repo/master/archives/ozone-client-cli-3.1.12.jar"
+  version "3.1.12"
+  sha256 "0094d04e3e449dacf25b18dc8405a0bfcadcd1cb8bece2458ea080425975384a"
 
 
   depends_on :java => "1.8+"
@@ -11,9 +11,9 @@ class Oz < Formula
   bottle :unneeded
 
   def install
-    jar = "ozone-client-cli-3.1.8.jar"
+    jar = "ozone-client-cli-3.1.12.jar"
     libexec.install jar
-    bin.write_jar_script libexec/"ozone-client-cli-3.1.8.jar", "oz"
+    bin.write_jar_script libexec/"ozone-client-cli-3.1.12.jar", "oz"
   end
 
   test do
