19e7e5194b297b9cd2d73c76010b16741a75e551
Antoine Duchateau <ad@taktik.com>
2017-11-20 16:53:45 +0100

    Added support for graphing items

--- Formula/oz.rb
+++ Formula/oz.rb
@@ -1,9 +1,9 @@
 class Oz < Formula
   desc "Ozone command line interface"
   homepage "https://www.taktik.com"
-  url "https://raw.githubusercontent.com/taktik/homebrew-repo/master/archives/ozone-client-cli-3.1.13.jar"
-  version "3.1.13"
-  sha256 "094f28ba1e020c9c93bcd817f939e9baec2e77ceb4e2f1ce850f5e61e083155a"
+  url "https://raw.githubusercontent.com/taktik/homebrew-repo/master/archives/ozone-client-cli-3.1.41.jar"
+  version "3.1.41"
+  sha256 "a4f7ef2a3dbd7e97ebb9835b982e0b6b5276f2e71abb53d6379111c5dec82d54"
 
 
   depends_on :java => "1.8+"
@@ -11,9 +11,9 @@ class Oz < Formula
   bottle :unneeded
 
   def install
-    jar = "ozone-client-cli-3.1.13.jar"
+    jar = "ozone-client-cli-3.1.41.jar"
     libexec.install jar
-    bin.write_jar_script libexec/"ozone-client-cli-3.1.13.jar", "oz"
+    bin.write_jar_script libexec/"ozone-client-cli-3.1.41.jar", "oz"
   end
 
   test do
