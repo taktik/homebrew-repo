513a31e28f629a5313fa6438ca947fd706d48ba5
Antoine Baudoux <ab@taktik.be>
2018-04-04 10:54:48 +0200

    Update oz.rb

--- Formula/oz.rb
+++ Formula/oz.rb
@@ -1,9 +1,9 @@
 class Oz < Formula
   desc "Ozone command line interface"
   homepage "https://www.taktik.com"
-  url "https://raw.githubusercontent.com/taktik/homebrew-repo/master/archives/ozone-client-cli-3.1.61.jar"
-  version "3.1.61"
-  sha256 "196caa61f6d78842a1579fa4ec4357f9a9cd2f50fbfd891e526f8ff5ed517e5d"
+  url "https://raw.githubusercontent.com/taktik/homebrew-repo/master/archives/ozone-client-cli-3.1.62.jar"
+  version "3.1.62"
+  sha256 "8506387a89d2b013e8f34ceb24c4eec82a0141204c4378ef03b155a95c97f108"
 
 
   depends_on :java => "1.8+"
@@ -11,9 +11,9 @@ class Oz < Formula
   bottle :unneeded
 
   def install
-    jar = "ozone-client-cli-3.1.61.jar"
+    jar = "ozone-client-cli-3.1.62.jar"
     libexec.install jar
-    bin.write_jar_script libexec/"ozone-client-cli-3.1.61.jar", "oz"
+    bin.write_jar_script libexec/"ozone-client-cli-3.1.62.jar", "oz"
   end
 
   test do
