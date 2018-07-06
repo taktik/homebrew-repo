118ac3f2b11f9144776139861cbe77a22912b190
Simon De Thuin <sdt@taktik.com>
2018-07-06 11:06:06 +0200

    WIP fix variable

--- Formula/oz.rb
+++ Formula/oz.rb
@@ -2,8 +2,8 @@ class Oz < Formula
   @@git_version = "3.1.81"
   desc "Ozone command line interface"
   homepage "https://www.taktik.com"
-  url "https://raw.githubusercontent.com/taktik/homebrew-repo/master/archives/ozone-client-cli-#{git_version}.jar"
-  version git_version
+  url "https://raw.githubusercontent.com/taktik/homebrew-repo/master/archives/ozone-client-cli-#{@@git_version}.jar"
+  version @@git_version
   sha256 "4b533d18e4d52aab65bdda4308c042d32c9d2fceedac37cd6019332a37118d4f"
 
 
@@ -12,9 +12,9 @@ class Oz < Formula
   bottle :unneeded
 
   def install
-    jar = "ozone-client-cli-#{git_version}.jar"
+    jar = "ozone-client-cli-#{@@git_version}.jar"
     libexec.install jar
-    bin.write_jar_script libexec/"ozone-client-cli-#{git_version}.jar", "oz"
+    bin.write_jar_script libexec/"ozone-client-cli-#{@@git_version}.jar", "oz"
   end
 
   test do
