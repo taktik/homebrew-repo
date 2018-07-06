1a04d74339b3b1353229cc8c9d157473d75047d4
Simon De Thuin <sdt@taktik.com>
2018-07-06 10:32:30 +0200

    Update oz with logs command version 3.1.81

--- Formula/oz.rb
+++ Formula/oz.rb
@@ -1,9 +1,9 @@
 class Oz < Formula
   desc "Ozone command line interface"
   homepage "https://www.taktik.com"
-  url "https://raw.githubusercontent.com/taktik/homebrew-repo/master/archives/ozone-client-cli-3.1.62.jar"
-  version "3.1.62"
-  sha256 "8506387a89d2b013e8f34ceb24c4eec82a0141204c4378ef03b155a95c97f108"
+  url "https://raw.githubusercontent.com/taktik/homebrew-repo/master/archives/ozone-client-cli-3.1.81.jar"
+  version "3.1.81"
+  sha256 "4b533d18e4d52aab65bdda4308c042d32c9d2fceedac37cd6019332a37118d4f"
 
 
   depends_on :java => "1.8+"
@@ -11,9 +11,9 @@ class Oz < Formula
   bottle :unneeded
 
   def install
-    jar = "ozone-client-cli-3.1.62.jar"
+    jar = "ozone-client-cli-3.1.81.jar"
     libexec.install jar
-    bin.write_jar_script libexec/"ozone-client-cli-3.1.62.jar", "oz"
+    bin.write_jar_script libexec/"ozone-client-cli-3.1.81.jar", "oz"
   end
 
   test do
