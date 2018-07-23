4922fcb6212a37a00f4633ce1c3b603dfa92d8ac
Simon De Thuin <sdt@taktik.com>
2018-07-23 15:05:35 +0200

    Bump version 3.1.84

--- Formula/oz.rb
+++ Formula/oz.rb
@@ -1,10 +1,10 @@
 class Oz < Formula
-  @@git_version = "3.1.83"
+  @@git_version = "3.1.84"
   desc "Ozone command line interface"
   homepage "https://www.taktik.com"
   url "https://raw.githubusercontent.com/taktik/homebrew-repo/master/archives/ozone-client-cli-#@@git_version.jar"
   version @@git_version
-  sha256 "be4e50e7432bb0ef7370846455146fde635378d41a231cb9244552c9f8f30487"
+  sha256 "5290ec2eb84578c0c96b7637000fd7b10eb38b58209290e6554e4467cef77eca"
 
 
   depends_on :java => "1.8+"
