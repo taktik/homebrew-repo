e73d4023fed0fd358b647675761c00c1412b9bb8
Simon3 <simonhardy8@gmail.com>
2019-04-19 12:05:34 +0200

    Update to 3.3-1565

--- Formula/oz.rb
+++ Formula/oz.rb
@@ -1,10 +1,10 @@
 class Oz < Formula
-  @@git_version = "3.3-1246-gaf59689cc0"
+  @@git_version = "feature-OZ-2972-3.3-1565-g1eb746681c"
   desc "Ozone command line interface"
   homepage "https://www.taktik.com"
   url "https://raw.githubusercontent.com/taktik/homebrew-repo/master/archives/client-cli-#@@git_version.jar"
-  version "3.3.1246"
-  sha256 "734b08376ecae24ff9d54e11caf9a87073f47229d004790fb3d242f9c5766720"
+  version "3.3-1565"
+  sha256 "04965aeaa0fb9f7e2fdf57ad8e756dc283991addf1379b29b8710fe1c8dae973"
 
 
   depends_on :java => "1.8+"
